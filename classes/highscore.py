73581c5be20c5852ceea97404c7af3f713734364
Avery <EverlastingBugstopper@users.noreply.github.com>
2016-07-08 14:01:35 -0400

    Add some error handling
    Checks if file name exists, and also won't break if there are less scores in the log file than requested

--- classes/highscore.py
+++ classes/highscore.py
@@ -1,5 +1,6 @@
 import csv
 from operator import itemgetter
+import sys
 
 class HighScores:
     def __init__(self, fileName, amount):
@@ -8,21 +9,38 @@ class HighScores:
         self._parse_file()
         
     def _parse_file(self):
-        with open(self.fileName, 'rt') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        try:
+            with open(self.fileName, 'rt') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',', quotechar='|')
             
-            highScores = []
-            for row in reader:
-                row[1] = int(row[1])
-                highScores.append(tuple(row))
-                highScores = sorted(highScores, key=itemgetter(1, 0), reverse=True)
+                highScores = []
+                for row in reader:
+                    row[1] = int(row[1])
+                    highScores.append(tuple(row))
+                    highScores = sorted(highScores, key=itemgetter(1, 0), reverse=True)
                 
-        for i, score in enumerate(highScores):
-            if i <= len(self.highScores) - 1:
-                self.highScores[i] = score
+            for i, score in enumerate(highScores):
+                if i <= len(self.highScores) - 1:
+                    self.highScores[i] = score
+        except FileNotFoundError:
+            print("File not found at", self.fileName)
+            raise
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
         
     def __str__(self):
         string = "File Name: %s\n\nAmount of High Scores: %s\n" % (self.fileName, len(self.highScores))
         for score in self.highScores:
-            string += "\n" + str(score[0]) + ": " + str(score[1])
+            try:
+                string += "\n" + str(score[0]) + ": " + str(score[1])
+            except TypeError:
+                if score is not None:
+                    print("Unexpected type error:", sys.exc_info()[0])
+                    raise
+                else:
+                    pass
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
+                raise
         return string
