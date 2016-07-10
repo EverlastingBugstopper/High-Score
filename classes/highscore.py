2a67ddabfb7fc28aa67de885a22dc36972a3d710
Avery Harnish <harnisha@lafayette.edu>
2016-07-10 10:29:41 -0400

    File name works on linux now, may have broken on Windows? Also added some more error handling, won't break if file is found, but is empty or in the wrong format.

--- classes/highscore.py
+++ classes/highscore.py
@@ -15,7 +15,7 @@ class HighScores:
             
                 highScores = []
                 for row in reader:
-                    row[1] = int(row[1])
+                    row[1] = float(row[1])
                     highScores.append(tuple(row))
                     highScores = sorted(highScores, key=itemgetter(1, 0), reverse=True)
                 
@@ -25,9 +25,41 @@ class HighScores:
         except FileNotFoundError:
             print("File not found at", self.fileName)
             raise
+        except IndexError:
+            print("No high scores found, or inappropriate file format")
+            pass
         except:
             print("Unexpected error:", sys.exc_info()[0])
             raise
+        
+    def _write_score(self, score):
+        try:
+            with open(self.fileName, 'w') as csvfile:
+                writer = csv.writer(csvfile, delimiter=',')
+                writer.writerow(score)
+        except FileNotFoundError:
+            print("File not found at", self.fileName)
+            raise
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise            
+        
+    def add_score(self, player, score):
+        try:
+            player = str(player)
+        except:
+            print("Player needs to be string:", sys.exc_info()[0])
+            raise
+
+        try:
+            score = float(score)
+        except:
+            print("Score could not be converted to a float.")
+            raise
+        scoreInfo = (player, score)
+        print(scoreInfo)
+
+        self._write_score(scoreInfo)
         
     def __str__(self):
         string = "File Name: %s\n\nAmount of High Scores: %s\n" % (self.fileName, len(self.highScores))
--- main.py
+++ main.py
@@ -2,11 +2,19 @@ from classes.highscore import HighScores
 import os
 
 def main():
-    directory = 'data' #path to directory containing data file
-    file = 'highscore.csv' #name of the file located in above directory
-    fileName = os.path.dirname(__file__) + os.path.normpath('/%s/%s' % (directory, file))
-    hs = HighScores(fileName, 10)
-    print(hs)
+	try:
+	    directory = 'data' #path to directory containing data file
+	    file = 'highscore.csv' #name of the file located in above directory
+	    fileName = os.path.abspath('%s/%s' % (directory, file))
+	    hs = HighScores(fileName, 10)
+	    print(hs)
+	except FileNotFoundError:
+		print("File not found at %s" % (fileName))
+		raise
+	except:
+		print("Unexpected error")
+		raise
 
 if __name__ == '__main__':
     main()
+    input("\nPress enter to continue. . .")
